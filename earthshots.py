ac53942fd6b50aec481987d4991ce9f2a23ddd91
Jacob Moore <billingsmoore@gmail.com>
2023-07-02 20:05:08 -0400

    started playing with angles

--- earthshots.py
+++ earthshots.py
@@ -11,13 +11,14 @@ file.close()
 driver = webdriver.Chrome()
 
 # iterate over coordinates and take screenshots
-for i in range(len(coords)//2):
-    
+for i in range(1): # just test on one coordinate
+    # get coordinate from list
     coord = coords[i][0].replace(' ', '')
     
+    # screenshot of coordinate from straight above
     url = 'https://earth.google.com/web/@' + coord + ',143000d'
-    
     driver.get(url)
-    time.sleep(10)
-
+    time.sleep(5)
     driver.save_screenshot('../earthshots-data/pics/' + coord.replace(',', '-') + '.png')
+
+    url_30 = url + ',30r'
