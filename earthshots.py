f1b877d5dfe710bc76b6eabb8630907b3c2fa15b
Jacob Moore <billingsmoore@gmail.com>
2023-07-02 21:02:18 -0400

    finished rotations

--- earthshots.py
+++ earthshots.py
@@ -2,11 +2,18 @@ from selenium import webdriver
 import time
 import csv
 
+# google earth uses url's in the format:
+# earth.google.com/web/@x,y,d,t,r
+# x is latitude, y is longitude
+# d is distance from the ground in meters
+# t and r are rotations around separate axes
+# all but x and y must contain the letter as a suffix on the numerical argument
+
 # time to sleep to let google earth load before taking a screenshot
-sleep = 10
+sleep = 6
 
 # read in coordinates as list
-file = open("earthshots-data/coordinates.csv", "r")
+file = open("../earthshots-data/coordinates.csv", "r")
 coords = list(csv.reader(file, delimiter=","))
 file.close()
 
@@ -19,19 +26,20 @@ for i in range(1): # just test on one coordinate
     coord = coords[i][0].replace(' ', '')
     
     # screenshot of coordinate from straight above
-    url = 'https://earth.google.com/web/@' + coord + ',143000d'
+    url = 'https://earth.google.com/web/@' + coord + ',60000d'
     driver.get(url)
     time.sleep(sleep)
-    driver.save_screenshot('../earthshots-data/pics/' + coord.replace(',', '-') + '.png')
 
-    # rotate around normal to Earth
-    for j in range(90, 271, 90):
+    # rotate t values
+    for j in range(0, 61, 15):
 
-        this_url = url + +',' + str(j) + 'r'
+        this_url = url +',' + str(j) + 't'
 
-        # screenshots from other angles
-        for k in range(30, 61, 15):
-            # screenshot from j degress
-            newrl = url + ',' + str(k) + 't'
+        # rotate r values
+        for k in range(0, 271, 90):
+
+            newrl = this_url + ',' + str(k) + 'r'
             driver.get(newrl)
             time.sleep(sleep)
+
+            driver.save_screenshot('../earthshots-data/pics/' + coord.replace(',', '-') + str(j) + '-' + str(k) + '.png')
